912645d959924e45445a01d9a856c28c1bc32d3b
Joao Beltrao <jb@joaobeltrao.com>
2021-03-07 07:42:27 +0000

    Added new error logic

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,7 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity) {
-        if (passengers<1 || Math.floor(passengers)!=passengers) throw "The number of passengers must be a positive integer value";
-        if (capacity<1 || Math.floor(capacity)!= capacity) throw "The capacity of the flight must be a positive integer value";
+        if (passengers<1 || Math.floor(passengers)!=passengers) throw new Error ("The number of passengers must be a positive integer value");
+        if (capacity<1 || Math.floor(capacity)!= capacity) throw new Error ("The capacity of the flight must be a positive integer value");
         let numberOfFlights = Math.floor(passengers/capacity);
         if (passengers % capacity !=0) {numberOfFlights++};
         return (numberOfFlights)
@@ -11,7 +11,7 @@ function Flights() {
         if (totalDistance <= distanceLimit/2) return ("The revision needs to be done within the next 3 months");
         if (totalDistance > distanceLimit/2 && totalDistance <= distanceLimit*0.75) return ("The revision needs to be done within the next 2 months");
         if (totalDistance > distanceLimit*0.74 && totalDistance <= distanceLimit) return ("The revision needs to be done within the next month");
-        if (totalDistance > distanceLimit) throw ("Total Distance is more than Distance Limit");
+        if (totalDistance > distanceLimit) throw new Error ("The maximum allowed flight distance should be exceeded");
     }
 
     return {calculateNumberOfFlights, checkAircraftRevision};
